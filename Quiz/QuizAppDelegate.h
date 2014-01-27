ee5406ee008a67c63ad47aa2e28ad4199f31a123
Arek Bolton <BoltronForce@Areks-MacBook-Pro.local>
2014-01-27 14:01:55 -0500

    Initial Commit

---
+++ Quiz/QuizAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  QuizAppDelegate.h
+//  Quiz
+//
+//  Created by Arek Bolton on 1/27/14.
+//  Copyright (c) 2014 Arek Bolton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuizAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Quiz/QuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  QuizViewController.h
+//  Quiz
+//
+//  Created by Arek Bolton on 1/27/14.
+//  Copyright (c) 2014 Arek Bolton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuizViewController : UIViewController
+
+@end
