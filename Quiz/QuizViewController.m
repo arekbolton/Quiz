57e6eddca05a422baac3323e1ae2834c9ef39580
arekbolton <arekboltonis@gmail.com>
2014-01-27 15:29:07 -0500

    Quiz app
    Limited functionality, having problems with getting the labels to work.
     To my understanding the buttons are doing their job.

--- Quiz/QuizViewController.m
+++ Quiz/QuizViewController.m
@@ -8,22 +8,44 @@
 
 #import "QuizViewController.h"
 
-@interface QuizViewController ()
-
-@end
-
 @implementation QuizViewController
 
-- (void)viewDidLoad
+-(id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    //Calling the init method of the superclass
+    self = [super initWithNibName:nibBundleOrNil bundle:nibBundleOrNil];
+    if(self) {
+        //creating two arrays an make the pointers point ot them
+        questions = [[NSMutableArray alloc] init];
+        answers = [[NSMutableArray alloc] init];
+        
+        //add questions and answers
+        [questions addObject:@"What is 7+7?"];
+        [answers addObject:@"14"];
+        
+        [questions addObject:@"What is the capitol of Vermont?"];
+        [answers addObject:@"Montipiler"];
+        
+        [questions addObject:@"From what is cognac made"];
+        [answers addObject:@"Grapes"];
+        
+    }
+    return self;
 }
-
-- (void)didReceiveMemoryWarning
+-(IBAction)showQuestion:(id)sender
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    currentQuestionIndex++;
+    if (currentQuestionIndex) {
+        currentQuestionIndex = 0;
+    }
+    
+    NSString *question = [questions objectAtIndex:currentQuestionIndex];
+    
+    NSLog(@"displaying question: %@", question);
+    
+    [questionField setText:question];
+    
+    [answerField setText:@"???"];
 }
 
 @end
